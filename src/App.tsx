a3d22c0ca7d76e3978e6ff9edce97d895df77dc5
Marcos Garcia <46078084+yamgarcia@users.noreply.github.com>
2020-09-12 23:31:34 -0700

    Update README

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import Header from "./components/Header";
 import GobalStyles from "./styles/global";
 import { ThemeProvider, DefaultTheme } from "styled-components";
